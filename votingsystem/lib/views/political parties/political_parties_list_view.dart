82cf19fc5d0cacabefb7316d5eafd53096c3982f
LinoMacKayIbm <33460472+ibmmacevedo@users.noreply.github.com>
2022-08-24 21:26:50 -0500

    adding success dialog

--- votingsystem/lib/views/political parties/political_parties_list_view.dart
+++ votingsystem/lib/views/political parties/political_parties_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:votingsystem/views/common/form_title.dart';
+import 'package:votingsystem/views/political%20parties/single_political_party.dart';
 
 import '../../models/political_party.dart';
 import '../common/form_button.dart';
@@ -83,140 +84,12 @@ class _PoliticalPartiesListViewState extends State<PoliticalPartiesListView> {
     );
   }
 
-  Widget singlePoliticalParty(index, screenWidth, screenHeight) {
-    var singlePolParty = politicalParties[index];
-    return GestureDetector(
-      onTap: () {
-        showDialog(
-            context: context,
-            builder: (ctx) {
-              return Dialog(
-                insetPadding: EdgeInsets.all(20),
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  width: screenWidth * 0.85,
-                  height: screenHeight * 0.45,
-                  child: Column(
-                    children: [
-                      Flexible(
-                        child: FormTitle(
-                          text: "Confirmación de Voto",
-                          fontSize: 28,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Wrap(
-                            alignment: WrapAlignment.center,
-                            children: [
-                              Text(
-                                "¿Seguro que desea votar por",
-                                style: TextStyle(fontSize: 18),
-                              ),
-                              Text(
-                                singlePolParty.leader,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 18),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            " de ",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Wrap(
-                            children: [
-                              Text(
-                                singlePolParty.name,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 18),
-                              ),
-                              Text(
-                                "?",
-                                style: TextStyle(fontSize: 18),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                      Expanded(child: SizedBox()),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          RoundedButtonWidget(
-                            width: screenWidth * 0.45,
-                            buttonText: "VOTAR",
-                            height: 50,
-                            onpressed: () {},
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.of(ctx).pop();
-                            },
-                            child: const Text(
-                              "Cancelar",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: const Color.fromRGBO(58, 60, 172, 1)),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              );
-            });
-      },
-      child: Card(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        elevation: 3,
-        child: ListTile(
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          dense: true,
-          title: Text(
-            politicalParties[index].leader.toUpperCase(),
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-          ),
-          subtitle: Text(politicalParties[index].name),
-          leading: SizedBox(
-            width: 50,
-            height: 50,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(politicalParties[index].logo),
-            ),
-          ),
-
-          // onTap: () {
-          //   Navigator.pushNamed(
-          //     context,
-          //     "/political_party_detail",
-          //     arguments: politicalParties[index],
-          //   );
-          // },
-        ),
-      ),
-    );
-  }
-
   Widget buildPoliticalParties(
       List<PoliticalParty> politicalParties, screenWidth, screenHeight) {
     return ListView.builder(
       itemCount: politicalParties.length,
       itemBuilder: (context, index) {
-        return singlePoliticalParty(index, screenWidth, screenHeight);
+        return SinglePoliticalParty(politicalParties[index]);
       },
     );
   }
